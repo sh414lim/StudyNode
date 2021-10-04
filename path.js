b1497102272d9d05a93bf897dea5168e7d485bf1
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-10-04 21:53:30 +0900

    path

--- path.js
+++ path.js
@@ -1,11 +1,18 @@
 //포괄적인 경로 지정을 위해 path 사용 
 const path= require('path');
 
+console.log(path.join(__dirname,".."))
+
+
+
 //윈도우 리눅스
-C:\user\zerocho
+// C:\user\zerocho
 
 //mac
-/user/zerocho
+// /user/zerocho
 
-path.join(__dirname,'var.js');
-\navigator.js
+//경로를 합쳐준다
+// path.join(__dirname,'var.js');
+
+// \node\lecture\var.js
+// posix 맥 리눅스
