6a2475153a2a8ceb41b8ac8922c64da9d0b95bbf
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-10-06 09:19:17 +0900

    cookie 로그인 로직

--- cookie2.js
+++ cookie2.js
@@ -17,16 +17,17 @@ http.createServer(async (req, res) => {
   const cookies = parseCookies(req.headers.cookie); // { mycookie: 'test' }
   // 주소가 /login으로 시작하는 경우
   if (req.url.startsWith('/login')) {
+      // query 에서 name을 추출
     const { query } = url.parse(req.url);
     const { name } = qs.parse(query);
     const expires = new Date();
     // 쿠키 유효 시간을 현재시간 + 5분으로 설정
     expires.setMinutes(expires.getMinutes() + 5);
-    res.writeHead(302, {
+    res.writeHead(302, { //redirect -> 다시 돌려보내라
       Location: '/',
       'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
-    });
-    res.end();
+    }); // 쿠키에 추가설정
+    res.end(); 
   // name이라는 쿠키가 있는 경우
   } else if (cookies.name) {
     res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
